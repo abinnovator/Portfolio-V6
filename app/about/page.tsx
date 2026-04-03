dfdc186568e505b3008b48ed6e7c7475467f7dac
Aadit Bhambri <abtheinnovator@gmail.com>
2026-04-03 14:05:12 +0530

    removed some animations

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,26 +4,12 @@ import { PixelGridShader } from '@/components/pixelgrid-shader';
 
 import React, { useRef } from 'react'
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import { SplitText } from 'gsap/SplitText';
 
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+
 
 const AboutPage = () => {
-  const containerRef = useRef<HTMLDivElement>(null)
-  const heroSplit = new SplitText(".title", { type: "chars, words" });
-    
-  useGSAP(() => {
-    gsap.from(heroSplit.chars, {
-      yPercent: 100,
-      duration: 1.8,
-      stagger: 0.06,
-      ease: "expo.out",
-    });
 
-  }, { scope: containerRef })
   return (
     /* Use min-h-screen to ensure the black background covers everything */
     <main className="relative bg-black w-full min-h-screen overflow-x-hidden">
@@ -36,8 +22,8 @@ const AboutPage = () => {
         <Navbar />
         <div className='pt-24'>
             <div className='flex flex-col md:flex-row gap-20 text-white text-center md:text-start'>
-                <h1 className='md:border-r-4 md:border-b-0 sm:border-b-4 px-7 md:text-8xl lg:text-9xl text-7xl title'>About me!</h1>
-                <p className='md:text-4xl text-2xl '>Hey! I&apos;m Aadit Bhambri. I&apos;mk a 13 year old full stack dev who mainly works with next.js and typescript. I love to making stuff and seeing new tech. I also have some experience with arduinos, raspberry pi&apos;s and esp&apos;s</p>
+                <h1 className='md:border-r-4 md:border-b-0 sm:border-b-4 px-7 md:text-8xl lg:text-9xl text-7xl'>About me!</h1>
+                <p className='md:text-4xl text-2xl title'>Hey! I&apos;m Aadit Bhambri. I&apos;mk a 13 year old full stack dev who mainly works with next.js and typescript. I love to making stuff and seeing new tech. I also have some experience with arduinos, raspberry pi&apos;s and esp&apos;s</p>
             </div>
         </div>
 
